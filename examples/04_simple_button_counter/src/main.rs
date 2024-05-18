5c25af4e327c64fda3806d21fd16ba11371bf75a
Jason Chin <cschin@infoecho.net>
2024-05-18 09:54:18 -0700

    add a new simple functional example

---
+++ examples/04_simple_button_counter/src/main.rs
@@ -0,0 +1,111 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+use askama::Template;
+use futures_util::Future;
+
+use axum::{extract::Json, http::HeaderMap, response::Html};
+//use serde::{Deserialize, Serialize};
+use tokio::sync::{mpsc::Sender, RwLock};
+
+use serde_json::Value;
+
+use tracing::debug;
+use tron_app::tron_components::{self, TnActionExecutionMethod, TnAsset, TnHtmlResponse};
+use tron_components::{
+    text::TnTextInput, TnButton, TnComponentBaseTrait, TnComponentState, TnComponentValue,
+    TnContext, TnContextBase, TnEvent, TnEventActions, TnTextArea,
+};
+//use std::sync::Mutex;
+use std::{collections::HashMap, pin::Pin, sync::Arc, task::Context};
+
+static BUTTON: &str = "button";
+
+// This is the main entry point of the application
+// It sets up the application configuration and state
+// and then starts the application by calling tron_app::run
+#[tokio::main]
+async fn main() {
+    let app_config = tron_app::AppConfigure::default();
+    // set app state
+    let app_share_data = tron_app::AppData {
+        context: RwLock::new(HashMap::default()),
+        session_expiry: RwLock::new(HashMap::default()),
+        event_actions: RwLock::new(TnEventActions::default()),
+        build_context: Arc::new(Box::new(build_context)),
+        build_actions: Arc::new(Box::new(build_actions)),
+        build_layout: Arc::new(Box::new(layout)),
+    };
+    tron_app::run(app_share_data, app_config).await
+}
+
+// These functions are used to build the application context,
+// layout, and event actions respectively
+fn build_context() -> TnContext {
+    let mut context = TnContextBase::default();
+
+    let component_index = 0;
+    let mut btn = TnButton::new(component_index, BUTTON.into(), "click me".into());
+    btn.set_attribute(
+        "class".to_string(),
+        "btn btn-sm btn-outline btn-primary flex-1".to_string(),
+    );
+
+    btn.set_attribute("hx-target".to_string(), "#count".to_string());
+    btn.set_attribute("hx-swap".to_string(), "innerHTML".to_string());
+    context.asset.blocking_write().insert("count".into(), TnAsset::U32(0));
+
+    context.add_component(btn);
+
+    TnContext {
+        base: Arc::new(RwLock::new(context)),
+    }
+}
+
+#[derive(Template)] // this will generate the code...
+#[template(path = "app_page.html", escape = "none")] // using the template in this path, relative                                    // to the `templates` dir in the crate root
+struct AppPageTemplate {
+    button: String,
+}
+
+fn layout(context: TnContext) -> String {
+    let context_guard = context.blocking_read();
+    let button = context_guard.render_to_string(BUTTON);
+    let html = AppPageTemplate { button };
+    html.render().unwrap()
+}
+
+fn build_actions(context: TnContext) -> TnEventActions {
+    let mut actions = TnEventActions::default();
+    let index = context.blocking_read().get_component_index(BUTTON);
+    actions.insert(
+        index,
+        (TnActionExecutionMethod::Await, Arc::new(button_clicked)),
+    );
+    actions
+}
+
+fn button_clicked(
+    context: TnContext,
+    event: TnEvent,
+    _payload: Value,
+) -> Pin<Box<dyn Future<Output = TnHtmlResponse> + Send + Sync>> {
+    let action = async move {
+        tracing::info!(target: "tron_app", "{:?}", event);
+        if event.e_trigger != BUTTON {
+            None
+        } else {
+            let asset_ref = context.get_asset_ref().await;
+            let mut asset_guard = asset_ref.write().await;
+            let count = asset_guard.get_mut("count").unwrap();
+            let new_count = if let TnAsset::U32(count) = count {
+                *count += 1;
+                *count
+            } else {
+                0
+            };
+            Some((HeaderMap::new(), Html::from(format!("count: {new_count}"))))
+        }
+    };
+    Box::pin(action)
+}

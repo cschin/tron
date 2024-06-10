51562059698308a458cd20596a9ccea11b6d018a
Jason Chin <cschin@infoecho.net>
2024-06-09 17:02:56 -0700

    change color map

--- examples/05_RAG_MAP/src/main.rs
+++ examples/05_RAG_MAP/src/main.rs
@@ -138,14 +138,14 @@ impl DocumentChunks {
 
 static CMAP: [&str; 97] = [
     "#870098", "#00aaa5", "#3bff00", "#ec0000", "#00a2c3", "#00f400", "#ff1500", "#0092dd",
-    "#00dc00", "#ff8100", "#007ddd", "#00c700", "#ffb100", "#0038dd", "#00af00", "#fcd200",
-    "#0000d5", "#009a00", "#f1e700", "#0000b1", "#00a55d", "#d4f700", "#4300a2", "#00aa93",
-    "#a1ff00", "#dc0000", "#00aaab", "#1dff00", "#f40000", "#009fcb", "#00ef00", "#ff2d00",
-    "#008ddd", "#00d700", "#ff9900", "#0078dd", "#00c200", "#ffb900", "#0025dd", "#00aa00",
     "#f9d700", "#0000c9", "#009b13", "#efed00", "#0300aa", "#00a773", "#ccf900", "#63009e",
     "#00aa98", "#84ff00", "#e10000", "#00a7b3", "#00ff00", "#f90000", "#009bd7", "#00ea00",
     "#ff4500", "#0088dd", "#00d200", "#ffa100", "#005ddd", "#00bc00", "#ffc100", "#0013dd",
     "#00a400", "#f7dd00", "#0000c1", "#009f33", "#e8f000", "#1800a7", "#00aa88", "#c4fc00",
+    "#00dc00", "#ff8100", "#007ddd", "#00c700", "#ffb100", "#0038dd", "#00af00", "#fcd200",
+    "#0000d5", "#009a00", "#f1e700", "#0000b1", "#00a55d", "#d4f700", "#4300a2", "#00aa93",
+    "#a1ff00", "#dc0000", "#00aaab", "#1dff00", "#f40000", "#009fcb", "#00ef00", "#ff2d00",
+    "#008ddd", "#00d700", "#ff9900", "#0078dd", "#00c200", "#ffb900", "#0025dd", "#00aa00",
     "#78009b", "#00aaa0", "#67ff00", "#e60000", "#00a4bb", "#00fa00", "#fe0000", "#0098dd",
     "#00e200", "#ff5d00", "#0082dd", "#00cc00", "#ffa900", "#004bdd", "#00b400", "#ffc900",
     "#0000dd", "#009f00", "#f4e200", "#0000b9", "#00a248", "#dcf400", "#2d00a4", "#00aa8d",
@@ -227,7 +227,7 @@ fn build_context() -> TnContext {
     );
     top_hit_div.set_attribute(
         "style".to_string(),
-        "resize:none; overflow: scroll;".to_string(),
+        "resize:none; overflow-y: auto;".to_string(),
     );
     context.add_component(top_hit_div);
 
@@ -287,7 +287,7 @@ fn build_context() -> TnContext {
     let mut query_stream_textarea =
         TnStreamTextArea::new(component_index, QUERY_STREAM_TEXTAREA.into(), Vec::new());
     query_stream_textarea.set_attribute("class".to_string(), "min-h-24 w-full".to_string());
-    query_stream_textarea.set_attribute("style".to_string(), "resize:none".to_string());
+    query_stream_textarea.set_attribute("style".to_string(), r#"resize:none"#.to_string());
     //query_stream_textarea.remove_attribute("disabled".into());
     context.add_component(query_stream_textarea);
 
